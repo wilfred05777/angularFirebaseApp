3f82d12a0bef2f004c315a7a102702ef24563e04
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-08 10:23:24 +0800

    Initial FrontEnd Angular Setup

--- frontendAngular/src/app/app.component.spec.ts
+++ frontendAngular/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularFrontend'`, () => {
+  it(`should have as title 'frontendAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularFrontend');
+    expect(app.title).toEqual('frontendAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularFrontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontendAngular app is running!');
   });
 });

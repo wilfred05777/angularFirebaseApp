acdb8938b30d334a974a381597e583cb729fbb54
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-08 10:42:51 +0800

    training: current, new and past training

--- frontendAngular/src/app/app.module.ts
+++ frontendAngular/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingComponent } from './training/past-training/past-training.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent
   ],
   imports: [
     MaterialModule,

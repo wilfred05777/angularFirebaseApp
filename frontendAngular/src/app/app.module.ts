9888a07e9ab864d7c6004ce36775cbe50d3d49fe
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-08 10:40:24 +0800

    auth: login and signup

--- frontendAngular/src/app/app.module.ts
+++ frontendAngular/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     MaterialModule,

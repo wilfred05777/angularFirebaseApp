cf445b5e7d879949c42b10df2d71674a16ab0b3f
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 06:55:34 +0800

    <mat-dialog-content> and const dialogRef

--- angularFrontend/src/app/training/current-training/stop-training.component.ts
+++ angularFrontend/src/app/training/current-training/stop-training.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-stop-training',
     template:   `
                 <h1 mat-dialog-title>Are you sure?</h1>
+                <mat-dialog-content>
+                    <p> You already got {{ passedData.progress }}% </p>
+                </mat-dialog-content>
                 <mat-dialog-actions>
                     <button mat-button [mat-dialog-close]="true">Yes</button>
                     <button mat-button [mat-dialog-close]="false">No</button>
@@ -11,4 +15,8 @@ import { Component } from '@angular/core';
                 `
 })
 
-export class StopTrainingComponent{}
+export class StopTrainingComponent{
+    constructor( @Inject(MAT_DIALOG_DATA) public passedData: any){
+
+    }
+}

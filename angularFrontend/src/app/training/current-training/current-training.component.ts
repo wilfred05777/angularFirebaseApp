d06dbc24a1dea88df89181abcec8f70e03b3a434
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 07:08:06 +0800

    comment: remaider and logic

--- angularFrontend/src/app/training/current-training/current-training.component.ts
+++ angularFrontend/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -8,12 +8,27 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  //exit
+  @Output() trainingExit = new EventEmitter();
+
   progress = 0;
   timer: number;
   
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
+    this.startOrResumeTimer();
+    // this.timer = setInterval(()=>{
+    //   this.progress = this.progress + 5;
+    //   if(this.progress >= 100) {
+    //     clearInterval(this.timer)
+    //   }
+    // }, 1000)
+
+  }
+
+  startOrResumeTimer(){
     this.timer = setInterval(()=>{
       this.progress = this.progress + 5;
       if(this.progress >= 100) {
@@ -29,8 +44,16 @@ export class CurrentTrainingComponent implements OnInit {
           progress: this.progress
         }
       });
+      
       dialogRef.afterClosed().subscribe(result =>{
-        console.log(result)
+        // Are you sure to quit: if selected is "yes" it will stop/terminated and go back to traning selectio
+        if(result){
+          this.trainingExit.emit();
+        } 
+        // Are you sure to quit: if selected is "No" it will resume and continue progress bar
+        else{
+          this.startOrResumeTimer();
+        }
       });
   }
 

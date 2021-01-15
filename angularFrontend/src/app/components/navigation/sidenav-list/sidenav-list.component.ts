50a7b1c991f989012adb76540a7150540b2df3f3
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 10:04:44 +0800

    implementing sidenav - authentication

--- angularFrontend/src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ angularFrontend/src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,19 +1,31 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../auth/auth.services';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
+  
   @Output() sidenavList = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus =>{
+      this.isAuth = authStatus;
+    });
   }
 
   onClose(){
     this.sidenavList.emit();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
   }
 } 

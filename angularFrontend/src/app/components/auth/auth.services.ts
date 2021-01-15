f5050fedf6da59209aad783fd28df5abe8ccb323
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 09:56:00 +0800

    Implementing Authentication - header

--- angularFrontend/src/app/components/auth/auth.services.ts
+++ angularFrontend/src/app/components/auth/auth.services.ts
@@ -1,7 +1,9 @@
+import { Subject } from "rxjs";
 import { AuthData } from "./auth-data.model";
 import { User } from "./user.model";
 
 export class AuthService {
+    authChange = new Subject<boolean>();
     private user: User
 
     registerUser(authData: AuthData){
@@ -9,21 +11,24 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random()* 10000).toString()
         };
+        this.authChange.next(true);
     }
 
     login(authData: AuthData){
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random()*10000).toString()
-        }
+        };
+        this.authChange.next(true);
     }
 
     logout(){
         this.user = null;
+        this.authChange.next(false);
     }
 
     getUser(){
-        return {...this.user };
+        return { ...this.user };
     }
 
     isAuth(){
--- angularFrontend/src/app/components/navigation/header/header.component.ts
+++ angularFrontend/src/app/components/navigation/header/header.component.ts
@@ -1,19 +1,32 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../auth/auth.services';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  @Output() sidenavToggle = new EventEmitter<void>();
+export class HeaderComponent implements OnInit, OnDestroy {
   
-  constructor() { }
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus =>{
+      this.isAuth = authStatus;
+    });
+
   }
 
   onToggle(){
     this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
   }
 }

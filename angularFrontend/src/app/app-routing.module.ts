35bce8bc30b2be85f000d6cc77d59a295a86c625
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 10:41:31 +0800

    Route Protection & authGuard

--- angularFrontend/src/app/app-routing.module.ts
+++ angularFrontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './components/auth/auth.guard';
+import { AuthService } from './components/auth/auth.services';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 // import { TrainingComponent } from './training/training.component';
@@ -10,11 +12,12 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'training', component: TrainingComponent},
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }

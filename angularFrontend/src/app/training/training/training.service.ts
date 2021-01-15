e3e314e2d1ea63cc400146b3157d41db91966138
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-15 11:30:02 +0800

    65. Setting an Active Exercise

--- angularFrontend/src/app/training/training/training.service.ts
+++ angularFrontend/src/app/training/training/training.service.ts
@@ -1,16 +1,23 @@
 import { Excercise } from './excercise.model';
 
 export class TrainingService {
-    public availableExercise: Excercise[] = [
+    private availableExercise: Excercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
         { id: 'touch-toes', name: 'Touch Toes', duration: 130, calories: 28},
         { id: 'side-lunges', name: 'Side Lunges', duration: 30, calories: 38},
         { id: 'burpess', name: 'Burpees', duration: 60, calories: 238}
     ];
 
+    private runningExcercise: Excercise;
+
     // reference type problematic object this one .slice() fixes  the issue
     getAvailableExcercises(){
         return this.availableExercise.slice();
+    }
+
+    startExcercise(selectedId: string){
+        const selectedExcercise = this.availableExercise.find(ex=> ex.id === selectedId)
+        this.runningExcercise = selectedExcercise;
     }
 }
 

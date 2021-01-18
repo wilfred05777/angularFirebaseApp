f08d3d7d7154a6fb63438d90a09e21c8e6454d42
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-19 04:25:23 +0800

    angular firebase and  config setup configuration

--- angularFrontend/src/environments/environment.prod.ts
+++ angularFrontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  config:{
+    apiKey: "AIzaSyDzM9Yium4d67ql90yQamaFD8D40oeECXk",
+    authDomain: "angularfirebaseapp-a66fe.firebaseapp.com",
+    projectId: "angularfirebaseapp-a66fe",
+    storageBucket: "angularfirebaseapp-a66fe.appspot.com",
+    messagingSenderId: "901113968965",
+    appId: "1:901113968965:web:23ef7d00dcc02f1226a62c",
+    measurementId: "G-PRNVVK0YNQ"
+  }
 };

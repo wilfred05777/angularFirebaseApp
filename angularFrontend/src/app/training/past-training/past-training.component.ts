8ba4f6d76d8b2301666ecd73259d1b2f5c26f377
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-18 19:15:14 +0800

    73. Adding Filtering to the Data Table

--- angularFrontend/src/app/training/past-training/past-training.component.ts
+++ angularFrontend/src/app/training/past-training/past-training.component.ts
@@ -24,7 +24,10 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(){
     this.dataSource.sort = this.sort;
-    
+
   }
 
+  doFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }

57c0b0a96f04300264543c8f6990dd598aa4cda6
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-25 18:44:24 +0800

    109. Loading a Module Lazily

---
+++ angularFrontend/src/app/training/training.routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../components/auth/auth.guard';
+import { TrainingComponent } from './training/training.component';
+
+const routes: Routes = [
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TrainingRoutingModule {}

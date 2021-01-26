b16d2d1c8cb297b0714a673b1cc2354fde599804
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-01-26 22:11:02 +0800

    119. Adding an Auth Reducer (and Actions) creation

--- angularFrontend/src/app/app.reducer.ts
+++ angularFrontend/src/app/app.reducer.ts
@@ -4,18 +4,23 @@ import {
   createSelector,
 } from '@ngrx/store';
 import * as fromUi from './shared/ui.reducer';
+import * as fromAuth from './components/auth/auth.reducer';
 
 export interface State {
   ui: fromUi.State;
+  auth: fromAuth.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
   ui: fromUi.uiReducer,
+  auth: fromAuth.authReducer,
 };
 
 export const getUiState = createFeatureSelector<fromUi.State>('ui');
 export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
 
+export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
+export const getIsAuth = createSelector(getAuthState, fromAuth.getIsAuth);
 // export interface State {
 //   // type definition
 //   isLoading: boolean;

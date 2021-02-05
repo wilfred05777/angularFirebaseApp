f67dd0825c57ba554ade534635ed55f432b5a186
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2021-02-06 05:51:52 +0800

    126. Connecting the Data Table - NgRx aproach

--- angularFrontend/src/app/training/past-training/past-training.component.ts
+++ angularFrontend/src/app/training/past-training/past-training.component.ts
@@ -9,8 +9,11 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+
 import { Excercise } from '../training/excercise.model';
 import { TrainingService } from '../training/training.service';
+import * as fromTraining from '../training/training.reducer';
 
 @Component({
   selector: 'app-past-training',
@@ -30,15 +33,25 @@ export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private store: Store<fromTraining.State>
+  ) {}
 
   ngOnInit(): void {
-    this.exChangedSubscription = this.trainingService.finishedExercisesChanged.subscribe(
-      (exercises: Excercise[]) => {
+    this.store
+      .select(fromTraining.getFinishedExcercises)
+      .subscribe((exercises: Excercise[]) => {
         this.dataSource.data = exercises;
-      }
-    );
+      });
     this.trainingService.fetchCompletedOrCancelledExcercise();
+
+    // this.exChangedSubscription = this.trainingService.finishedExercisesChanged.subscribe(
+    //   (exercises: Excercise[]) => {
+    //     this.dataSource.data = exercises;
+    //   }
+    // );
+    // this.trainingService.fetchCompletedOrCancelledExcercise();
     // this.dataSource.data = this.trainingService.getCompletedOrCancelledExcercise();
   }
 
